edda7e39c67f27f3dd68596943aa260eaa2f1b1f
helen <hhma@alumni.cpp.edu>
2019-06-19 21:44:08 -0400

    add check map size = 1

--- src/test/java/guru/springframework/JUnitExtensionTest.java
+++ src/test/java/guru/springframework/JUnitExtensionTest.java
@@ -1,11 +1,14 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Map;
+
+import static org.mockito.Mockito.when;
 
 /**
  * Created by jt on 2018-10-29.
@@ -18,6 +21,10 @@ public class JUnitExtensionTest {
 
     @Test
     void testMock() {
+
         mapMock.put("keyvalue", "foo");
+        when(mapMock.size()).thenReturn(1);
+        assertEquals(1, mapMock.size());
+        Assertions.assertEquals(1, mapMock.size());
     }
 }
